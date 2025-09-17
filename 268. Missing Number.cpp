b8c0d5daf1e7860f4d6033cdde8408f9fe4029b3
siddhardharoy11 <155885300+siddhardharoy11@users.noreply.github.com>
2025-09-17 11:45:01 +0530

    Create 268. Missing Number.cpp

---
+++ 268. Missing Number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int left=0;
+        int right = nums.size();
+        while(left<right){
+            int mid = left+(right-left)/2;
+            if(nums[mid]>mid){
+                right = mid;
+            }
+            else{
+                left = mid+1;
+            }
+
+
+        }
+        return left;
+    }
+    
+};
